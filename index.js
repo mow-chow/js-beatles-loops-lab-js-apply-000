e2414048fb27756e48753bde61b6bca7719dabdf
Catherine Cherubin <cat.cherubin@gmail.com>
2019-04-17 13:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // add solution here
+//first look at index-test.js to see correct information
 
 var musicians = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"];
 var instruments = ["Guitar", "Bass GUitar", "Drums", "Lead Guitar"];
-
+//defined variables with array of names and instrumetns
 
 function theBeatlesPlay(musicians, instruments) {
   var array = []
@@ -11,6 +12,9 @@ function theBeatlesPlay(musicians, instruments) {
       }
       return array
 }
+//create empty array
+//create function that accepts two arguments, iterating through each array and
+//crafting a sentance (interpolation) before adding each completed sentance into the empty array
 
 function johnLennonFacts(facts) {
   var facts = [
@@ -25,6 +29,10 @@ while (i < 4) {
   array.push(facts[i] + "!!!")
   i++
 }
+//create a variable that holds an array of facts
+//create an empty array
+//create function that accepts one argument, facts array, and loops through the array adding a string
+//returns the new array
 return array
 }
 
@@ -37,3 +45,6 @@ function iLoveTheBeatles(number) {
 while (number < 15)
 return array
 }
+//create an empty array
+//create a function that accepts one argument, a number, runs a do while loop that pushes a string into the empty array
+//until it satisfies the established conditions
